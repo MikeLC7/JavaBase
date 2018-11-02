5b9eceef99849a78ac680482adf5bea4ecc0cad6
mikelc7 <244788816@qq.com>
2018-11-02 11:10:33 +0800

    complete the Queue

---
+++ src/main/java/JavaBase/Collection/List/LinkedList/README.md
@@ -0,0 +1,14 @@
+------
+# LinkedListDemoListA
+
+------
+## 示例
+### ArrayListDemoListA
+#### simpleDemo
+- 简单示例
+
+------
+## 目录
+###
+
+------
---
+++ src/main/java/JavaBase/DataStructure/Queue/QueueDemoListA.java
@@ -0,0 +1,26 @@
+package JavaBase.DataStructure.Queue;
+
+
+import JavaBase.DataStructure.Stack.implement.optimizeMode.ArrayStack;
+import org.junit.Test;
+
+/**
+ * Project:
+ *
+ * File: StackDemoListA
+ *
+ * Description:
+ *
+ * @author: MikeLC
+ *
+ * @date: 2018/11/2 上午 09:30
+ *
+ * Copyright ( c ) 2018
+ *
+ */
+public class QueueDemoListA {
+
+
+
+
+}
