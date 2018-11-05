5df347b2f3b938699379b1960e9c6d00f6ed7562
mikelc7 <244788816@qq.com>
2018-11-06 00:13:14 +0800

    ADD the JVM classLoad loadType catalog

---
+++ src/main/java/JVM/classLoad/loadType/CommonClass.java
@@ -0,0 +1,20 @@
+package JVM.classLoad.loadType;
+
+/**
+ * Project:
+ *
+ * File: CommonClass
+ *
+ * Description:
+ *
+ * @author: MikeLC
+ *
+ * @date: 2018/11/6 上午 12:02
+ *
+ * Copyright ( c ) 2018
+ *
+ */
+public class CommonClass {
+
+
+}
---
+++ src/main/java/JavaBase/base/DemoList.java
@@ -0,0 +1,35 @@
+package JavaBase.base;
+
+
+import org.junit.Test;
+
+/**
+ * Project:
+ *
+ * File: DemoList
+ *
+ * Description:
+ *
+ * @author: MikeLC
+ *
+ * @date: 2018/11/5 下午 11:38
+ *
+ * Copyright ( c ) 2018
+ *
+ */
+public class DemoList {
+
+    class TestClass{
+
+    }
+
+    @Test
+    public void demo1(){
+        TestClass testClass = new TestClass();
+        System.out.println(testClass.getClass());
+        System.out.println(TestClass.class);
+        System.out.println(testClass.getClass().equals(TestClass.class));
+        System.out.println(testClass.getClass() == TestClass.class);
+    }
+
+}
